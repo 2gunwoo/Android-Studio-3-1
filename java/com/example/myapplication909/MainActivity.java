c1ceb6a7ed348bc74e552371e5e4115b82234a9c
2gunwoo <106566831+2gunwoo@users.noreply.github.com>
2022-12-14 14:01:39 +0900

    로그인 기능, 화면

---
+++ java/com/example/myapplication909/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.myapplication909;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et_id, et_pw;
+    private Button btn_login, btn_register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTitle("Safe Lab");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_DENIED){
+            Log.i("SafeLab","문자 보내기 권한이 없습니다. 사용자에게 부여를 요청합니다.");
+            String[] permissions = {Manifest.permission.SEND_SMS};
+            ActivityCompat.requestPermissions(this,permissions, MODE_PRIVATE);
+        }
+
+        et_id = findViewById(R.id.et_id);
+        et_pw = findViewById(R.id.et_pw);
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, com.example.myapplication909.MainActivity8.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = et_id.getText().toString();
+                String pw = et_pw.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if(success){
+                                String id = jsonObject.getString("id");
+                                String pw = jsonObject.getString("pw");
+                                Toast.makeText(getApplicationContext(), "로그인이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                                intent.putExtra("id", id);
+                                intent.putExtra("pw", pw);
+                                startActivity(intent);
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(), "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(id, pw, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                queue.add(loginRequest);
+            }
+        });
+    }
+}
